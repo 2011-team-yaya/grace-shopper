8623846e6aeb5dcb38554d2134b8571fc49f1595
ele <53164633+EL-E-org@users.noreply.github.com>
2021-01-18 19:22:37 -0500

    fix: gatekeepers.js

--- server/api/gatekeepers.js
+++ server/api/gatekeepers.js
@@ -1,13 +1,3 @@
-//middleware for admin only
-// const adminsOnly = (req, res, next) => {
-//   if (!req.user.isAdmin) {
-//     const error = new Error('User is Not an Admin: Request Denied!')
-//     error.status = 401
-//     return next(error)
-//   }
-//   next()
-// }
-
 const adminsOnly = (req, res, next) => {
   const currentUser = req.user
   if (currentUser && currentUser.isAdmin) {
